5c174dc14ed30c4654d650381f31d52fe6588916
xaccrocheur(fz) <xaccrocheur@gmail.com>
2012-08-02 21:43:26 +0000

    Image tests

---
+++ tests-image.el
@@ -0,0 +1,34 @@
+(defconst smiley-data '(#x00 #x00 #x24 #x00 #x00 #x24 #x18 #x00))
+
+(defun smiley-from-xbm-data ()
+  ;; Make an XBM image from the smiley data
+  (list 'image :type 'xbm :ascent 100 :data
+        ))
+
+
+(defimage test-image
+	((:type xpm :file "/usr/share/pixmaps/xscreensaver.xpm")))
+
+(defun imapua-px-insert-image (image type)
+	"Make an image using the bytes directly in a string"
+(insert-image (create-image image type nil :heuristic-mask t))
+
+;; 	(list 'image :ascent 100
+;; 				;; :data image
+;; 				:type type
+;; 				:file "/usr/share/icons/rosa/status/16/keys.xpm"
+;; )
+	)
+
+(insert-image (imapua-px-image-from-string "/usr/share/apps/akonadi/contact/pics/world.jpg" "jpeg"))
+
+(insert-image (imapua-px-image-from-string "/usr/share/pixmaps/xscreensaver.xpm" "xbm"))
+
+(insert-image test-image)
+
+;; this works
+(insert-image (create-image "/usr/share/pixmaps/xscreensaver.xpm" 'xpm nil :heuristic-mask t))
+
+;; this too
+(insert-image (create-image "/usr/share/apps/akonadi/contact/pics/world.jpg" 'jpeg nil :heuristic-mask t))
+
